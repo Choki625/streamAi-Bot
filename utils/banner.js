abc8aa53a4156e044610c48fff54881ad597aa11
Choki625 <muhammadhawi123@gmail.com>
2024-12-18 21:21:18 +0700

    Initial commit

---
+++ utils/banner.js
@@ -0,0 +1,8 @@
+const banner = `
+ ============================================
+|             streamAI Network Bot           |
+|               Airdrop Vault                |
+|         https://t.me/airdropvault_id       |
+|             github.com/Choki625            |
+ =============================================`
+export default banner;
